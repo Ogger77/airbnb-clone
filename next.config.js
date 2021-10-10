8075627d9a7c09b4c6879043755565baf9301ba6
ogger77 <vud@kean.edu>
2021-10-10 00:41:38 -0400

    added mapbox

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1Ijoib2dnZXIiLCJhIjoiY2tmZGY3ZHJmMDJlbzJybXl6eXk0NDVqbSJ9.L9GRoQwVi_i-IUh8p3gbZg",
+  },
 };
